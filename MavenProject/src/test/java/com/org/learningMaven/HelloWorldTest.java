2405d7973ac39a95aa128684d7952495ca32c05b
Arundeep Singh Chohan <arundeepchohan2009@hotmail.com>
2020-10-09 09:29:05 -0700

    Add files via upload

---
+++ MavenProject/src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.org.learningMaven;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+	@Test
+	public void Login() {
+		// TODO Auto-generated method stub
+		System.out.println("Login");
+	}
+
+}
